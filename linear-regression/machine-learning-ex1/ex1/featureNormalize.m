3bfa44be9e9668880c556cb186d5661135a928f5
SamKirkiles <sam.kirkiles@gmail.com>
2017-08-31 15:06:37 -0400

    feature normailization now multiple dimensions

--- linear-regression/machine-learning-ex1/ex1/featureNormalize.m
+++ linear-regression/machine-learning-ex1/ex1/featureNormalize.m
@@ -26,10 +26,10 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-mu = mean(X(:,1:2))
-sigma = std(X_norm(:,1:2))
+mu = mean(X_norm)
+sigma = std(X_norm)
 
-X_norm(:,1:2) = (X(:,1:2) .- mu) ./ sigma
+X_norm = (X_norm .- mu) ./ sigma
 
 
 
