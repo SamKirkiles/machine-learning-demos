7c3cc0abe2f74a1f70f214cb4a289d91c044e2cc
SamKirkiles <sam.kirkiles@gmail.com>
2017-12-09 20:39:33 -0500

    removed metadata

---
+++ standalone-projects/driver-status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 24 19:31:24 2017
+
+@author: samkirkiles
+"""
+
+import numpy as np
+import pandas as pd
+
+data = np.loadtxt('New_Driver_Application_Status.csv', delimiter=",")   
