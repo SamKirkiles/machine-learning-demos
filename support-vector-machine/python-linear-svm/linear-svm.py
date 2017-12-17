d332b8417b77930d5ba324dcdaf91c7cfd0e00bc
SamKirkiles <sam.kirkiles@gmail.com>
2017-12-17 01:15:07 -0500

    added support vector machine python implementation with linear decision boundary not kernalized yet

---
+++ support-vector-machine/python-linear-svm/linear-svm.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+from sklearn.datasets import make_classification;
+import matplotlib.pyplot as plt;
+import numpy as np;
+from cvxopt.solvers import qp;
+import cvxopt.solvers;
+
+# Langerian svm on data set without kernels
+
+# Create training data with two features and binary classification
+X,y = make_classification(n_samples=100, n_redundant=0, class_sep=1.0, n_informative=1, n_features=2, n_clusters_per_class=1, random_state=5);
+
+#Show a scatter plot of our data
+plt.scatter(X[:,0][np.where(y == 1.0)], X[:,1][np.where(y == 1.0)], marker='o', c='#4e71c9');
+plt.scatter(X[:,0][np.where(y == 0.0)], X[:,1][np.where(y == 0.0)], marker='o', c='#b53636');
+
+m = X.shape[0];
+y = np.reshape(y,(m,1));
+
+# We must find the extrema of the function in the form 1/2*a'Pa-q'x subject to constraints
+  
+Py = y.dot(y.T);
+Px = X.dot(X.T);
+P = cvxopt.matrix(Py * Px);
+
+q = cvxopt.matrix(-1.0 * np.ones((m)));
+
+G = cvxopt.matrix(np.diag(-1.0 * np.ones((m))));
+h = cvxopt.matrix(np.zeros((m)));
+
+A = cvxopt.matrix(y.reshape(1,m).astype(float));
+b = cvxopt.matrix(0.0);
+
+solution = qp(P,q, G, h, A, b);
+
+multipliers = np.ravel(solution['x']);
+
+# We need to only use the support vectors 
+has_positive_multiplier = multipliers > 1e-7
+sv_multipliers = multipliers[has_positive_multiplier]
+support_vectors = X[has_positive_multiplier]
+support_vectors_y = y[has_positive_multiplier]
+
+# Now that we have our alpha values we must compute w and b to find our line
+
+def compute_w(multipliers, X, y):
+    return np.sum(multipliers[i] * y[i] * X[i] for i in range(len(y)))
+
+normw = compute_w(multipliers, X, y);
+w = compute_w(sv_multipliers, support_vectors, support_vectors_y)
+
+def compute_b(w, X, y):
+    return np.sum([y[i] - np.dot(w, X[i]) for i in range(len(X))])/len(X)
+
+b = compute_b(w,X,y);
+
+p = ((X.dot(w)) >= 0).reshape(m,1);
+
+plt.show();
+
+print('Final Accuracy: ', np.mean(p == y) * 100)
+
+
+
