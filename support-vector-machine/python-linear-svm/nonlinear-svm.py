9d5a173c6d4652045245779d425eec39ae746945
SamKirkiles <sam.kirkiles@gmail.com>
2017-12-23 15:04:31 -0500

    vectorized svm

--- support-vector-machine/python-linear-svm/nonlinear-svm.py
+++ support-vector-machine/python-linear-svm/nonlinear-svm.py
@@ -7,6 +7,11 @@ from cvxopt.solvers import qp;
 import cvxopt.solvers;
 from datetime import datetime 
 
+# Change parameters here
+C = 0.4;
+poly_kernel = False
+vectorized = True
+
 
 # Langerian svm on data set with kernels
 # This project uses very similar code to the linear-svm project but uses a nonlinear decision boundary
@@ -16,7 +21,6 @@ from datetime import datetime
 X,y = make_gaussian_quantiles(mean=None, cov=1.0, n_samples=100, n_features=2, n_classes=2, shuffle=True, random_state=5);
 
 y[y==0] = -1
-C = 0.4;
 
 #Show a scatter plot of our data
 plt.scatter(X[:,0][np.where(y == 1.0)], X[:,1][np.where(y == 1.0)], marker='o', c='#4e71c9');
@@ -25,8 +29,6 @@ plt.scatter(X[:,0][np.where(y == -1.0)], X[:,1][np.where(y == -1.0)], marker='o'
 m = X.shape[0];
 y = np.reshape(y,(m,1));
 
-poly_kernel = False
-
 # We must find the extrema of the function in the form 1/2*a'Pa-q'x subject to constraints
 
 # Kernel function if poly is true will compute polynomial kernel otherwise gaussian
@@ -34,19 +36,37 @@ poly_kernel = False
 print("Training...")
 startTime= datetime.now() 
 
-
-def kernel(a,b,d=20,poly=True,sigma=0.5):
+def kernel(a,b,d=20,poly=True,sigma=0.5,):
     if (poly):
         return np.inner(a,b) ** d;
     else:
-        return np.exp(-np.linalg.norm((a - b) ** 2)/sigma**2)
+        return np.exp(-np.sqrt(np.sum(((a-b) ** 4), axis=0))/sigma**2)
+    
+def kernel_v(X,Y,d=20,poly=True,sigma=0.5,gram=False):
+    if (poly):
+        return np.einsum('ij,kj->ik',X,Y) ** d;
+    elif (gram):
+        X2 = X * X
+        Y2 = Y * Y
+        T = np.einsum('ik,jk',X2,6 * Y2) - np.einsum('ik,jk',X2*X,4*Y) - np.einsum('ik,jk',X ,4*Y2*Y)
+        T += np.add.outer(np.einsum('ij,ij->i',X2,X2),np.einsum('ij,ij->i',Y2,Y2));
+        return np.exp(-np.sqrt(np.maximum(0,T))/sigma ** 2);
+    else:
+        s = np.sum((np.subtract(X,Y) ** 4), axis=1)[:,None]
+        return np.exp(-np.sqrt(s)/sigma**2)
 
+    
 
-# K must be a gram matrix whichi is all possible combinations of X and y
+# K must be a gram matrix which is all possible combinations of X and y
 
-K = np.array([kernel(X[i], X[j],poly=poly_kernel) 
+if (vectorized):
+    K = kernel_v(X,X,poly=poly_kernel,gram=True);
+else:
+    K = np.array([kernel(X[i], X[j],poly=poly_kernel) 
     for j in range(m)   
     for i in range(m)]).reshape((m, m))
+
+
 
 P = cvxopt.matrix(y.dot(y.T) * K);
 q = cvxopt.matrix(-1.0 * np.ones((m)));
@@ -97,7 +117,7 @@ for i in range(len(X)):
 p = np.sign(p + b)  
 
 p = p.reshape(p.shape[0],1)
-
+        
 # TODO: Vectorize this method
 #Plot decision boundary
 print("Accuracy: ", np.mean(p == y) * 100)
@@ -111,16 +131,24 @@ print("Plotting decision boundary...");
 
 x_contour = np.linspace(np.min(X[:,0]),np.max(X[:,0]),200);
 y_contour = np.linspace(np.min(X[:,1]),np.max(X[:,1]),200);
-
 z = np.zeros((len(x_contour),len(y_contour)))
 
-# do another one of these gram matrices
-for i in range(len(x_contour)):
-    for j in range(len(y_contour)):
-        s = 0; 
-        for a, sv_y, sv in zip(sv_multipliers, support_vectors_y, support_vectors):
-            s += a * sv_y * kernel(np.array([x_contour[i],y_contour[j]]), sv, poly=poly_kernel)
-        z[i][j] = np.sign(s + b)
+# do another one of these gram matrices or vectorized otherwise
+
+if (vectorized):
+    for i in range(len(x_contour)):
+        for j in range(len(y_contour)):
+            T = (sv_multipliers * support_vectors_y * kernel_v(np.array([x_contour[i],y_contour[j]]).reshape(2,1).T, support_vectors, poly=poly_kernel))
+            s = np.sum(T, axis=0)
+            z[i][j] = np.sign(s + b)
+else:   
+    for i in range(len(x_contour)):
+        for j in range(len(y_contour)):
+            s = 0; 
+            for a, sv_y, sv in zip(sv_multipliers, support_vectors_y, support_vectors):
+                s += a * sv_y * kernel(np.array([x_contour[i],y_contour[j]]), sv, poly=poly_kernel)
+            z[i][j] = np.sign(s + b)
+
             
 plt.contour(x_contour,y_contour,z.T,[0]);
 
