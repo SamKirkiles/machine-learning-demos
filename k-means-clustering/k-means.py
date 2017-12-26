87447021e6e2c3a7e739dbcf474d83bd7a873760
SamKirkiles <sam.kirkiles@gmail.com>
2017-12-25 19:28:24 -0500

    fixed k-means loop bug

--- k-means-clustering/k-means.py
+++ k-means-clustering/k-means.py
@@ -7,7 +7,7 @@ import numpy as np;
 k = 2
 
 #We don't need the y because we are using unsupervised learning
-X,y = make_blobs(n_samples=100,n_features=2,centers=k,cluster_std=1.0,random_state=9)
+X,y = make_blobs(n_samples=100,n_features=2,centers=k,cluster_std=1.0,random_state=2)
 
 c = X[np.random.randint(X.shape[0],size=k)]
 m = X.shape[0]
@@ -24,30 +24,21 @@ plt.scatter(c[:,0:1],c[:,1:2], c=color_sequence, s=150)
 plt.show();
 
 
-
 for i in range(0,1000):
     
     tempc = c
     
-    #Issue is coming from here
+    for j in range(0,k):
+        X_out[:,j] = np.sqrt(np.sum(((X[:,:]-c[j,:])**2),axis=1))
+        
+    y_out[:,0] = np.asarray([X_out[:,0]>X_out[:,1]])        
+    y_out[:,1] = np.asarray([X_out[:,1]>X_out[:,0]])
     
     for j in range(0,k):
-        X_out[:,j] = np.sum(np.sqrt(np.subtract(X,tempc[j,0:2])**4),axis=1)
-        y_out[:,0] = np.asarray([X_out[:,0]>X_out[:,1]])
-        y_out[:,1] = np.asarray([X_out[:,1]>X_out[:,0]])
         tempc[j,:] = np.mean(X[y_out[:,j]][:,0:2],axis=0)
-        
+    
     c = tempc
     
-    if i%100 == 0:
-        for i in range(0,k):
-            plt.scatter(X[y_out[:,i]][:,0:1], X[y_out[:,i]][:,1:2], c=color_sequence[i])
-            
-        plt.scatter(c[:,0:1],c[:,1:2], c=color_sequence, s=150)
-        plt.show();
-        
-
-
 # set centroids to average of clusters
     
 # If any blue shows up, there are unlabeled points
