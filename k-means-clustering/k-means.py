330bd55efc53bf18e834964cd92bc794c115662a
SamKirkiles <sam.kirkiles@gmail.com>
2017-12-26 22:56:21 -0500

    added cost function and graph for kmeans also annotated initial centroids and result graph

--- k-means-clustering/k-means.py
+++ k-means-clustering/k-means.py
@@ -7,7 +7,7 @@ import numpy as np;
 k = 2
 
 #We don't need the y because we are using unsupervised learning
-X,y = make_blobs(n_samples=100,n_features=2,centers=k,cluster_std=1.0,random_state=50)
+X,y = make_blobs(n_samples=100,n_features=2,centers=k,cluster_std=2,random_state=10)
 
 c = X[np.random.randint(X.shape[0],size=k)]
 m = X.shape[0]
@@ -17,19 +17,28 @@ color_sequence = ['g','r']
 X_out = np.zeros((m,k))
 y_out = np.empty((m, k), dtype=bool)
 
-print('original c')
-print(c)
+plt.title('Initial Centroids')
 plt.scatter(X[:,0:1],X[:,1:2])
 plt.scatter(c[:,0:1],c[:,1:2], c=color_sequence, s=150)
 plt.show();
 
+def cost_function(X,idx,ce):
+    cost = 0
+    for j in range(0,k):
+        cost += np.sum(np.sqrt(np.sum((X[idx[:,j]] - ce[j,:])**2,axis=1)),axis=0)
+
+            
+    return cost/m
+
+cost = np.zeros((100,1))
+plot = 0;
 
 for i in range(0,1000):
     
     tempc = c
     
     for j in range(0,k):
-        X_out[:,j] = np.sum(np.sqrt(((X[:,:]-c[j,:])**2)),axis=1)
+        X_out[:,j] = np.sqrt(np.sum(((X[:,:]-c[j,:])**2),axis=1))
         
     y_out[:,0] = np.asarray([X_out[:,0]>X_out[:,1]])        
     y_out[:,1] = np.asarray([X_out[:,1]>X_out[:,0]])
@@ -39,10 +48,18 @@ for i in range(0,1000):
     
     c = tempc
     
+    # Information for cost graph not important
+    if i < 100:
+        cost[plot] = cost_function(X,y_out,c)
+        plot += 1;
+
+    
 # set centroids to average of clusters
     
 # If any blue shows up, there are unlabeled points
 
+# Plot result
+plt.title('Final Clusters')
 plt.scatter(X[:,0:1],X[:,1:2],c='b')
 
 for i in range(0,k):
@@ -50,4 +67,11 @@ for i in range(0,k):
 
 plt.scatter(c[:,0:1],c[:,1:2], c=color_sequence, s=150)
 
+plt.show()
+
+# Plot cost
+plt.subplot()
+plt.plot(cost)
+plt.xlabel('Iterations')
+plt.ylabel('Cost')
 plt.show()
